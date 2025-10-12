b136ac9b6d48e1e4cc3be55441caf44806612d42
Sergio Augusto Celis Esteban <s.celise@uniandes.edu.co>
2025-10-11 22:27:23 -0500

    Se crea el front para el cierre de sesión y se integra la funcionalidad de cambio de idioma

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../utils/colors_app.dart';
+
 import '../utils/responsive_app.dart';
 
 import '../widgets/general_widgets/poppins_text.dart';
+import '../widgets/general_widgets/drawer_menu_widget.dart';
 
 class HomePage extends StatelessWidget {
+  
   const HomePage(
     {
       super.key = const Key('home_page')
@@ -15,6 +18,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: DrawerMenuWidget(),
+      appBar: AppBar(
+        leading: Builder(
+          builder: ( context ) => IconButton(
+            onPressed: () => Scaffold.of(context).openDrawer(),
+            icon: Icon(
+              Icons.menu_rounded,
+              color: ColorsApp.secondaryColor,
+              semanticLabel: 'Menu'
+            )
+          )
+        ),
+        title: PoppinsText(
+          sText: 'MediSupply',
+          dFontSize: ResponsiveApp.dSize( 20.0 ),
+          colorText: ColorsApp.secondaryColor,
+          fontWeight: FontWeight.w500
+        ),
+        backgroundColor: ColorsApp.backgroundColor,
+        elevation: 0
+      ),
       body: Center(
         child: PoppinsText(
           sText: 'Bienvenido a MediSupply',
